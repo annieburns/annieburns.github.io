1032d92753a26dbce44206d6be87edc681cfd5aa
annie burns <anniebnyc@gmail.com>
2016-10-21 17:10:15 -0400

    website update

--- README.md
+++
@@ -1,8 +0,0 @@
-# Annie Burns
-
-# 3 Goals
-# - Learn to Code
-# - Change Career Path
-# - Network & Make Friends
-
-![elephant] (Nature-Animals-Cute-Little-Baby-Elephant.jpg)
---
+++ app.js
@@ -0,0 +1,34 @@
+
+
+
+ function showhideAbout() {
+    const div = document.getElementById("aboutMe");
+    if (div.style.display !== "none") {
+        div.style.display = "none";
+    }
+    else {
+        div.style.display = "block";
+    }
+     }
+
+
+ function showhideWork() {
+    const div = document.getElementById("work");
+    if (div.style.display !== "none") {
+        div.style.display = "none";
+    }
+    else {
+        div.style.display = "block";
+    }
+     }
+
+
+ function showhideContact() {
+    const div = document.getElementById("contact");
+    if (div.style.display !== "none") {
+        div.style.display = "none";
+    }
+    else {
+        div.style.display = "block";
+    }
+     }
