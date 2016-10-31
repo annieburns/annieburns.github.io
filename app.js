3cec30640347319d94ee1c91ddcdcce6b90dc54c
annie burns <anniebnyc@gmail.com>
2016-10-31 19:33:45 -0400

    website update

--- app.js
+++ app.js
@@ -1,34 +1,46 @@
 
+window.onload = function() {
+  document.getElementById("button1").addEventListener('click', showAbout);
+  document.getElementById("buttonX").addEventListener('click', hideAbout);
+  document.getElementById("button2").addEventListener('click', showWork);
+  document.getElementById("buttonX2").addEventListener('click', hideWork);
+
+function showAbout() {
+  const div = document.getElementById("aboutMe");
+   if (div.style.display !== "none") {
+      div.style.display = "none";
+  }
+  else {
+      div.style.display = "block";
+  }
+   }
+
+function hideAbout() {
+  const div = document.getElementById("aboutMe");
+    div.style.display = "none";
+}
 
 
- function showhideAbout() {
-    const div = document.getElementById("aboutMe");
-    if (div.style.display !== "none") {
-        div.style.display = "none";
-    }
-    else {
-        div.style.display = "block";
-    }
-     }
+function showWork() {
+  const div = document.getElementById("work");
+  if (div.style.display !== "none") {
+      div.style.display = "none";
+  }
+  else {
+      div.style.display = "block";
+  }
+   }
 
 
- function showhideWork() {
-    const div = document.getElementById("work");
-    if (div.style.display !== "none") {
-        div.style.display = "none";
-    }
-    else {
-        div.style.display = "block";
-    }
-     }
+function hideWork() {
+  const div = document.getElementById("work");
+    div.style.display = "none";
+ }
+}
 
 
- function showhideContact() {
-    const div = document.getElementById("contact");
-    if (div.style.display !== "none") {
-        div.style.display = "none";
-    }
-    else {
-        div.style.display = "block";
-    }
-     }
+
+
+
+
+
